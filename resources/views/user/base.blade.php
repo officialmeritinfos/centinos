06bc5ad3fd98890fedef5e683078bf8c8e788032
Anon Dev <michaelerastus9@gmail.com>
2024-10-15 07:43:47 +0100

    Removed former livechat

--- resources/views/user/base.blade.php
+++ resources/views/user/base.blade.php
@@ -280,17 +280,6 @@
         new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
     }
 </script>
-<!-- Smartsupp Live Chat script -->
-<script type="text/javascript">
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = '6f30e1675c7c8c2cc5321c1f14ccc11d9837fdd6';
-window.smartsupp||(function(d) {
-  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-  c.type='text/javascript';c.charset='utf-8';c.async=true;
-  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
-</script>
-<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+
 </body>
 </html>
